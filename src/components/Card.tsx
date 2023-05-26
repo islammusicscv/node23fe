25b8eaab2f4f627aa1cda76bfbd57ae929ff2071
Islam Mušić <islam.music@scv.si>
2023-05-26 09:38:15 +0200

    izpis blogov

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-const Card = () => {
+const Card = ({cardData}) => {
   return (
       <>
           <div className="col">
@@ -10,9 +10,7 @@ const Card = () => {
                       <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
                   </svg>
                   <div className="card-body">
-                      <p className="card-text">This is a wider card with supporting text below as a
-                          natural lead-in to additional content. This content is a little bit
-                          longer.</p>
+                      <p className="card-text">{cardData.content}</p>
                       <div className="d-flex justify-content-between align-items-center">
                           <div className="btn-group">
                               <button type="button" className="btn btn-sm btn-outline-secondary">View
